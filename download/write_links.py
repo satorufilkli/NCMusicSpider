cf61f01c19adfc378bc23c44f4c029a55d2917af
satorufilkli <satorufilkli@pm.me>
2024-12-12 15:56:12 +0800

    fix : add mp3 format support

--- download/write_links.py
+++ download/write_links.py
@@ -1,10 +1,18 @@
-def write_links_to_txt(dl_urls, filename="./download/links.txt"):
+def write_links_to_txt(songs_with_url, filename="./download/links.txt"):
     try:
         with open(filename, 'w', encoding='utf-8') as file:
-            for dl_url in dl_urls:
-                file.write(f"{dl_url['url']} ")
-                file.write(f"{dl_url['name']}-{dl_url['artist']}.flac\n")
-                print(f"正在写入歌曲 {dl_url['name']} 信息")
+            for song_with_url in songs_with_url:
+                url = song_with_url['url']
+                if '.mp3' in url.lower():
+                    format = '.mp3'
+                elif '.flac' in url.lower():
+                    format = '.flac'
+                else:
+                    format = '.mp3'
+
+                file.write(f"{url} ")
+                file.write(f"{song_with_url['name']} - {song_with_url['artist']}{format}\n")
+                print(f"正在写入歌曲 {song_with_url['name']} 信息")
         print(f"成功将歌曲信息写入 {filename}")
 
     except Exception as e:
